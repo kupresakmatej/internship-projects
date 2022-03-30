69507034d4aff0f59773129f6d62b5309987ee6d
Matej Kuprešak <matej.kupresak@fpxstudio.com>
2022-03-30 12:52:43 +0200

    fpx

--- FPXzadatak1/FPXzadatak1/Output.cs
+++ FPXzadatak1/FPXzadatak1/Output.cs
@@ -33,11 +33,6 @@ namespace FPXzadatak1
             int rowLength = Logic.Instance.BoardLayout.GetLength(0);
             int columnLength = Logic.Instance.BoardLayout.GetLength(1);
 
-            //string writeLine = " --- --- --- --- --- --- ---";
-
-            int cursorPositionTop = Console.CursorTop;
-            int cursorPositionLeft = Console.CursorLeft;
-
             for (int i = 0; i < 6; i++)
             {
                 Console.WriteLine(" --- --- --- --- --- --- ---");
@@ -45,62 +40,35 @@ namespace FPXzadatak1
             }
             Console.WriteLine(" --- --- --- --- --- --- ---");
 
-            cursorPositionTop = Console.CursorTop - 1;
-            cursorPositionLeft = Console.CursorLeft;
+            int cursorPositionTop = Console.CursorTop;
+            int cursorPositionLeft = Console.CursorLeft;
 
-            //for (int i = 0; i < rowLength; i++)
-            //{
-            //    Console.CursorTop = (cursorPositionTop - 1) + i;
-            //    Console.CursorLeft = cursorPositionLeft + 1;
-
-            //    for (int j = 0; j < columnLength; j++)
-            //    {
-            //        if (Logic.Instance.BoardLayout[i, j] != Coin.Empty)
-            //        {
-            //            if (Logic.Instance.BoardLayout[i, j] == Coin.PlayerA)
-            //            {
-            //                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.firstPlayer.Color);
-            //            }
-            //            else if (Logic.Instance.BoardLayout[i, j] == Coin.PlayerB)
-            //            {
-            //                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.secondPlayer.Color);
-            //            }
-            //            Console.Write(string.Format(" 0 ").Insert(cursorPositionLeft, ""));
-            //            Console.ForegroundColor = ConsoleColor.White;
-            //            Console.Write("|");
-            //        }
-            //        else
-            //        {
-            //            Console.ForegroundColor = ConsoleColor.White;
-            //            Console.Write(string.Format(""));
-            //        }
-            //    }
-            //    Console.ForegroundColor = ConsoleColor.White;
-            //}
-
-            for(int i = 0; i < rowLength - 1; i++)
+            for (int i = 0; i < rowLength; i++)
             {
-                Console.CursorTop = cursorPositionTop - 1;
+                Console.CursorTop = (cursorPositionTop - 2) + (i - 5);
                 Console.CursorLeft = cursorPositionLeft + 1;
 
-                if (Logic.Instance.BoardLayout[i, cursorPositionTop + i] != Coin.Empty)
+                for (int j = 0; j < columnLength; j++)
                 {
-                    if (Logic.Instance.BoardLayout[i, cursorPositionTop + i] == Coin.PlayerA)
+                    if (Logic.Instance.BoardLayout[i, j] != Coin.Empty)
                     {
-                        Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.firstPlayer.Color);
+                        if (Logic.Instance.BoardLayout[i, j] == Coin.PlayerA)
+                        {
+                            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.firstPlayer.Color);
+                        }
+                        else if (Logic.Instance.BoardLayout[i, j] == Coin.PlayerB)
+                        {
+                            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.secondPlayer.Color);
+                        }
+                        Console.Write(string.Format(" 0 ").Insert(cursorPositionLeft, ""));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("|");
                     }
-                    else if (Logic.Instance.BoardLayout[i, cursorPositionTop + i] == Coin.PlayerB)
+                    else
                     {
-                        Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Gameplay.secondPlayer.Color);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(string.Format(""));
                     }
-                    Console.Write(string.Format(" 0 ").Insert(cursorPositionLeft, ""));
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("|");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(string.Format(""));
                 }
                 Console.ForegroundColor = ConsoleColor.White;
             }
