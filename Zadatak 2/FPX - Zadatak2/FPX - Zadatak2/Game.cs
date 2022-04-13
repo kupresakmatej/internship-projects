aebd793ed363a55520356e5fde60fe00283ee89d
Matej Kuprešak <matej.kupresak@fpxstudio.com>
2022-04-13 10:41:02 +0200

    fpx

---
+++ Zadatak 2/FPX - Zadatak2/FPX - Zadatak2/Game.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace FPX___Zadatak2
+{
+    public class Game
+    {
+        private double theta = 0.0;
+        GameWindow gameWindow;
+
+        public Game(GameWindow window)
+        {
+            this.gameWindow = window;
+            Start();
+        }
+
+        public void Start()
+        {
+            gameWindow.Load += Loaded;
+            gameWindow.Resize += Resize;
+            gameWindow.RenderFrame += RenderF;
+            gameWindow.Run(1.0 / 60.0); //time in seconds after which the window updates
+        }
+
+        public void Resize(object o, EventArgs e)
+        {
+            GL.Viewport(0, 0, gameWindow.Width, gameWindow.Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        public void RenderF(object o, EventArgs e)
+        {
+            GL.LoadIdentity();
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Rotate(theta, 0.0, 0.0, 1.0);
+            GL.Begin(BeginMode.Quads);
+
+            GL.Color3(1.0, 0.0, 0.0);
+            GL.Vertex2(30.0, 30.0);
+            GL.Color3(0.0, 1.0, 0.0);
+            GL.Vertex2(-30.0, 30.0);
+            GL.Color3(0.0, 0.0, 1.0);
+            GL.Vertex2(-30.0, -30.0);
+            GL.Color3(1.0, 1.0, 0.0);
+            GL.Vertex2(30.0, -30.0);
+
+            GL.End();
+
+            gameWindow.SwapBuffers();
+
+            theta += 1.0; //update se ne radi ovako inace, updateframe funkcija
+            if(theta > 360)
+            {
+                theta -= 360;
+            }
+        }
+
+        public void Loaded(object o, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+    }
+}
--- Zadatak 2/FPX - Zadatak2/FPX - Zadatak2/Program.cs
+++ Zadatak 2/FPX - Zadatak2/FPX - Zadatak2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
 
 namespace FPX___Zadatak2
 {
@@ -10,6 +11,8 @@ namespace FPX___Zadatak2
     {
         static void Main(string[] args)
         {
+            GameWindow window = new GameWindow(500, 500);
+            Game game = new Game(window);
         }
     }
 }
