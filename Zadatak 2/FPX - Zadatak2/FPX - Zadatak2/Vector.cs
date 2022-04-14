caa2ce2919cb49f46c6bdad6e2e09b031945c3af
Matej Kuprešak <matej.kupresak@fpxstudio.com>
2022-04-14 11:59:54 +0200

    fpx

---
+++ Zadatak 2/FPX - Zadatak2/FPX - Zadatak2/Vector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPX___Zadatak2
+{
+    public struct Vector
+    {
+        private double X;
+        private double Y;
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+    }
+}
